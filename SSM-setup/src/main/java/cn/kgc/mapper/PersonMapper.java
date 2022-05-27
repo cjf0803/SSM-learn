35185ae8ade6b0b1bbe1394516858a80f70dc97a
cjf0803 <532055876@qq.com>
2022-05-27 20:04:17 +0800

    2022.5.27

--- SSM-setup/src/main/java/cn/kgc/mapper/PersonMapper.java
+++ SSM-setup/src/main/java/cn/kgc/mapper/PersonMapper.java
@@ -1,7 +1,32 @@
 package cn.kgc.mapper;
 
+import cn.kgc.entity.Person;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface PersonMapper {
     //定义查询记录方法数
     public Integer findCount();
+    //增加
+    public Integer addPerson(Person person);
+    //修改功能
+    public Integer updatePerson(Person person);
+    //删除功能
+    public Integer delById(Integer Id);
+    //删除功能2
+    public Integer delById2(Person person);
+    //查询对象
+    public Person findById(Integer Id);
+    //查询集合
+    public List<Person> findAll();
+    //模糊查询
+    public List<Person> findByName(String name);
+    //通过用户名和昵称 绝对查询
+    public List<Person> findByPerson(Person person);
+    //通过用户名和昵称 绝对查询2
+    public List<Person> findByPerson2(@Param("name")String name,@Param("nickname")String nickname);
+    //测试￥（不安全）符号和#（安全）的作用
+    public List<Person> findColList(@Param("colname")String colname);
 }
 
